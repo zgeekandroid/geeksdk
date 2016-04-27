f3e89f3bae4aac2218a05c01cd9c6f2cf2043485
hsy719007146 <719007146@qq.com>
2016-04-27 16:06:51 +0800

    add common library

---
+++ commonslibrary/src/main/java/com/geekandroid/sdk/sample/commonslibrary/utils/ToastUtils.java
@@ -0,0 +1,18 @@
+package com.geekandroid.sdk.sample.commonslibrary.utils;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.widget.Toast;
+
+/**
+ * date        :  2016-01-27  13:27
+ * author      :  Mickaecle gizthon
+ * description :
+ */
+public class ToastUtils {
+    public static void show(Context context,String message) {
+        Toast toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0, 10);
+        toast.show();
+    }
+}
